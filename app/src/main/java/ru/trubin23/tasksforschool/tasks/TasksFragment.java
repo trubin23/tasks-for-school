dcdb0a2f4e025066fd8db93d77af9ca62809ed64
Andrey Trubin <trubin23@gmail.com>
2018-03-29 23:58:19 +0500

    adding a TasksFragment and TasksPresenter interaction

--- app/src/main/java/ru/trubin23/tasksforschool/tasks/TasksFragment.java
+++ app/src/main/java/ru/trubin23/tasksforschool/tasks/TasksFragment.java
@@ -1,7 +1,15 @@
 package ru.trubin23.tasksforschool.tasks;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import ru.trubin23.tasksforschool.R;
 
 /**
  * Created by Andrey on 29.03.2018.
@@ -10,6 +18,14 @@ import android.support.v4.app.Fragment;
 public class TasksFragment extends Fragment implements TasksContract.View {
 
     private TasksContract.Presenter mPresenter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tasks_frag, container, false);
+        ButterKnife.bind(this, root);
+        return root;
+    }
 
     @Override
     public void setPresenter(@NonNull TasksContract.Presenter presenter) {
