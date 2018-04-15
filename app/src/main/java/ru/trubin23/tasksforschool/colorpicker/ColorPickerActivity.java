dd6158ee640ea463a90770c09a9e5bb40006a2ad
Andrey Trubin <trubin23@gmail.com>
2018-04-15 23:41:36 +0500

    start implementation ColorPicker

---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorPickerActivity.java
@@ -0,0 +1,15 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.trubin23.tasksforschool.R;
+
+public class ColorPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.colorpicker_act);
+    }
+}
---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorPickerPresenter.java
@@ -0,0 +1,8 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+/**
+ * Created by Andrey on 15.04.2018.
+ */
+
+public class ColorPickerPresenter {
+}
