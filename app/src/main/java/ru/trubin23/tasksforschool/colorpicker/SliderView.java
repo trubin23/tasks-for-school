80e29407f3c29c3fbbeaed35dcec991b830191c1
Andrey Trubin <trubin23@gmail.com>
2018-04-19 23:59:00 +0500

    adding SliderView to layout colorpicker_frag

---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/SliderView.java
@@ -0,0 +1,25 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Andrey on 19.04.2018.
+ */
+
+public class SliderView extends View {
+
+    public SliderView(Context context) {
+        super(context);
+    }
+
+    public SliderView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SliderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
