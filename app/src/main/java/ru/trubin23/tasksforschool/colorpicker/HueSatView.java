34a60080dd8a8be0dcb5d2b7d0253e07f4fcc206
Andrey Trubin <trubin23@gmail.com>
2018-04-19 23:42:53 +0500

    change layout colorpicker_frag for HSV color model

---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/HueSatView.java
@@ -0,0 +1,24 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Andrey on 19.04.2018.
+ */
+
+public class HueSatView extends View {
+    public HueSatView(Context context) {
+        super(context);
+    }
+
+    public HueSatView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HueSatView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/SwatchView.java
@@ -0,0 +1,24 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Andrey on 19.04.2018.
+ */
+
+public class SwatchView extends View {
+    public SwatchView(Context context) {
+        super(context);
+    }
+
+    public SwatchView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwatchView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
