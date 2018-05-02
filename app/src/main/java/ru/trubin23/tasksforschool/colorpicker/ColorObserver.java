6960f9385186ff752d38380b538a7e72c1ac3908
Andrey Trubin <trubin23@gmail.com>
2018-05-02 21:30:44 +0500

    removing part of the ColorPicker components for future match to the task

--- app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorObserver.java
+++
@@ -1,9 +0,0 @@
-package ru.trubin23.tasksforschool.colorpicker;
-
-/**
- * Created by Andrey on 25.04.2018.
- */
-
-interface ColorObserver {
-    void updateColor(ObservableColor observableColor);
-}
