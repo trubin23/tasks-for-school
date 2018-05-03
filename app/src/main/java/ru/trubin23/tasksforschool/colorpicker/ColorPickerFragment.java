79dcb3964713bc2b56001c144556c5e25314fe81
Andrey Trubin <trubin23@gmail.com>
2018-05-03 21:58:55 +0500

    adding a background for ColorPicker according to Hue from the HSV

--- app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorPickerFragment.java
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorPickerFragment.java
@@ -1,13 +1,18 @@
 package ru.trubin23.tasksforschool.colorpicker;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.trubin23.tasksforschool.R;
 
@@ -18,6 +23,9 @@ import ru.trubin23.tasksforschool.R;
 public class ColorPickerFragment extends Fragment implements ColorPickerContract.View {
 
     private ColorPickerContract.Presenter mPresenter;
+
+    @BindView(R.id.color_picker)
+    ConstraintLayout mConstraintLayout;
 
     public static ColorPickerFragment newInstance() {
         return new ColorPickerFragment();
@@ -30,7 +38,23 @@ public class ColorPickerFragment extends Fragment implements ColorPickerContract
         View root = inflater.inflate(R.layout.colorpicker_frag, container, false);
         ButterKnife.bind(this, root);
 
+        int[] colors = buildHueColorArray();
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT,
+                colors);
+        mConstraintLayout.setBackground(gradientDrawable);
+
         return root;
+    }
+
+    private int[] buildHueColorArray() {
+        int[] hue = new int[361];
+        int count = 0;
+        for (int i = hue.length - 1; i >= 0; i--, count++)
+        {
+            hue[count] = Color.HSVToColor(new float[]{i, 1f, 1f});
+        }
+        return hue;
     }
 
     @Override
---
+++ app/src/main/java/ru/trubin23/tasksforschool/colorpicker/ColorSquare.java
@@ -0,0 +1,20 @@
+package ru.trubin23.tasksforschool.colorpicker;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by Andrey on 03.05.2018.
+ */
+
+public class ColorSquare extends AppCompatImageView {
+    public ColorSquare(Context context) {
+        super(context);
+    }
+
+    public ColorSquare(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
